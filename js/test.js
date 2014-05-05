af5cd1bdc9b69260f0d9d0eccf433cdeec4125d0
Shakir Khan <shakirkhan@shakirs-mbp.default>
2014-05-05 17:22:12 +0100

    Initial commit

---
+++ js/test.js
@@ -0,0 +1,3 @@
+function Name () {
+	var name = 'Shakir';
+}
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+class MyClass {
+	/**
+	 * [run description]
+	 * @param  string $person
+	 * @param  [type] $direction
+	 * @param  [type] $speed
+	 * @return [type]
+	 */
+	public function run($person='shakir', $direction, $speed)
+	{
+		return "Some code would go there";
+	}
+}
